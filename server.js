213d0bbf5a19deb0258f70d89f3acfa7b890a20b
alexreu <alexxxx.974@gmail.com>
2018-06-26 09:14:41 +0400

    modification de la fonction qui affiche le prenom

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require ('express');
 const app = express();
 const port = 3012;
 
+// moteur de rendu ejs
 app.set('view engine', 'ejs');
 
+// route  qui appel une fonction qui fait un rendu sur index.ejs
 app.get('/', function (req, res) {
-    var name = "alexandre";
-    res.render('index', {
-        data: displayname(name)
-    })
+    displayname(res)
 });
 
+// fonction qui affiche un prénom #middleware
+function displayname (res){
+    var name = "alexandre";
+    res.render('index', {
+        data: name
+    });
+}
+
+// port d'écoute du serveur
 app.listen(port, function () {
     console.log("Server ON / Port : " + port);
 });
-
-function displayname (name){
-    return name;
-}
